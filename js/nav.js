0d2a56612f80bc310dd4aa1e53a4e208737d8ce0
Neel Patel <patelneel1732@gmail.com>
2021-06-20 19:26:29 +0530

    companyhouse

---
+++ js/nav.js
@@ -0,0 +1,35 @@
+//Show and hide hamburguer menu in small screens 
+const btn = document.querySelector('.mobile-menu-button');
+const menu = document.querySelector('.mobile-menu');
+const close = document.querySelector('.close');
+
+btn.addEventListener("click", () => {
+    menu.classList.toggle('hidden');
+});
+
+close.addEventListener("click", () => {
+    menu.classList.toggle('hidden');
+});
+
+// window.addEventListener('scroll',() => {
+//    var navbar = document.querySelector('.navbar');
+//    navbar.classList.toggle('stick', window.scrollY > 0);
+// });
+// const menu = document.getElementById("close");
+// // const ulMenu = document.getElementById("close");
+
+// function menuToggle() {
+//     menu.classList.toggle('h-32')
+// }
+
+// // Browser resize listener
+// window.addEventListener("resize", menuResize);
+
+// // Rezise menu if user changing the width with responsive menu opened
+// function menuResize() {
+//     // first get the size from the window
+//     const window_size = window.innerWidth || document.body.clientWidth;
+//     if (window_size > 640){
+//         menu.classList.remove('h-32');
+//     }
+// }
