2d4891468ddaf791f158abb7aab2810198daac9c
Neel Patel <patelneel1732@gmail.com>
2021-06-22 10:22:12 +0530

    companyhouse

--- js/nav.js
+++ js/nav.js
@@ -2,18 +2,26 @@
 const btn = document.querySelector('.mobile-menu-button');
 const menu = document.querySelector('.mobile-menu');
 const close = document.querySelector('.close');
+const home = document.querySelector(".topnav");
+
 
 btn.addEventListener("click", () => {
     menu.classList.toggle('hidden');
+    home.classList.toggle('mt-60');
+    
 });
 
 close.addEventListener("click", () => {
     menu.classList.toggle('hidden');
+    home.classList.toggle('mt-60');
 });
 
+
 // window.addEventListener('scroll',() => {
-//    var navbar = document.querySelector('.navbar');
-//    navbar.classList.toggle('stick', window.scrollY > 0);
+//     console.log(window.scrollY);
+
+// //    var navbar = document.querySelector('.navbar');
+// //    navbar.classList.toggle('stick', window.scrollY > 0);
 // });
 // const menu = document.getElementById("close");
 // // const ulMenu = document.getElementById("close");
